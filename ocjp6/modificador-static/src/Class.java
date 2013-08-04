eea2ecb0255228fa6ce34e1e7cd8c5ac6c8aae4d
César Barbosa <cesar@vesso.com.br>
2013-08-04 20:31:57 -0300

    Adic. ocjp6/modificador-static

---
+++ ocjp6/modificador-static/src/Class.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * <h1>Class</h1> 
+ * <p>created on: Aug 4, 2013, 8:16:41 PM.</p>
+ *
+ * @author César Barbosa <cesar@vesso.com.br>
+ */
+class Jao {
+    public static void main(String[] args) {
+        System.out.println("testando Jao...");
+    }
+}
+class Maria {
+    public static void main(String[] args) {
+        System.out.println("testando Maria...");
+    }
+}
