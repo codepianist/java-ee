0517e12bdb36671417ab16be79d7322358277248
César Barbosa <cesar@vesso.com.br>
2013-08-07 13:57:49 -0300

    ocjp6/src/com/codepianist/ocjp6/m_overloading/

---
+++ ocjp6/src/com/codepianist/ocjp6/m_overloading/boxing/WidenAndBox.java
@@ -0,0 +1,24 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.codepianist.ocjp6.m_overloading.boxing;
+
+/**
+ * <h1>WidenAndBox</h1> 
+ * <p>created on: Aug 6, 2013, 5:38:39 PM.</p>
+ *
+ * @author César Barbosa <cesar@vesso.com.br>
+ */
+public class WidenAndBox {
+    static void doSomething(Long l){
+        System.out.println("Long");
+    }
+    public static void main(String[] args) {
+        long l = 150;
+        doSomething(l);
+        byte b = 5;
+        //doSomething(b); // trying to widen than box -- illegal
+    }
+}
